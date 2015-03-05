38706d361ccfa3fb1074e0a9e9c3582c314e2577
Jen Wachter <jenleighkelly@gmail.com>
2015-03-05 13:49:52 -0500

    Changed bin script run by composer on update or install.

--- public/index.php
+++ public/index.php
@@ -1,3 +1,7 @@
 <?php
 
-require dirname(__DIR__) . "/bootstrap.php";
+$root = dirname(__DIR__);
+
+define("WP_USE_THEMES", true);
+require $root . "/vendor/autoload.php";
+require $root . "/vendor/wordpress/wordpress/wp-blog-header.php";
